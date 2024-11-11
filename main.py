b2b398dcb688d3197d8e9b97d2f79196d3ff67e9
Derek Arreaga <drk.fab@gmail.com>
2024-11-11 03:06:36 -0600

    Se cambiaron las máquinas a la misma pero con distinta cadena para los resultados diferentes
    Se agrega el campo de descripción a la máquina para mostrarla en la consola

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ if __name__ == "__main__":
     config_files = [
         ("configAccept.yaml", "simulation_output_accept.txt"),
         ("configReject.yaml", "simulation_output_reject.txt"),
-        ("configLoop.yaml", "simulation_output_loop.txt")
+        ("configLoop.yaml", "simulation_output_loop.txt"),
+        #Configuracion para las maquinas de ejemplos adicionales
+        #("config.yaml", "simulation_testing.txt")
     ]
     
     key_replacements = {
@@ -19,21 +21,22 @@ if __name__ == "__main__":
     for config_file, output_file in config_files:
         print(f"\nSimulating with configuration: {config_file}")
         
-        # Load the configuration and apply replacements
+        # Cargar la configuración y reemplazar los símbolos leidos incorrectamente
         config = load_config(config_file)
         for old_key, new_key in key_replacements.items():
             if old_key in config:
                 config[new_key] = config.pop(old_key)
         
-        # Instantiate TuringMachine and Simulator with the configuration
+        # Instancia la Máquina y el simulador con la configuración
         tm = TuringMachine(config)
         simulator = Simulator(tm)
 
-        # Get the input string for the simulation
         cadena = config.get("cadena", "")
         if not cadena:
             cadena = input("Ingrese una cadena para la simulación: ")
         
-        # Run the simulation and save the result in the specified output file
+        # Ejecutar la simulación y guardar el resultado en un archivo .txt
+        if config.get("description", "") != "":
+                print(f"Descripción: {config.get("description", "")}")
         result = simulator.simulate(cadena, output_file=output_file)
         print(f"Result for {config_file}: {result}")
